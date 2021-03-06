d474e7885863f37092e54fd3079e11e84e5112f7
マツモト・ユウノスケ <matsumotoyuunosuke@matsumotoyuunosukenoMacBook-Air.local>
2021-03-06 20:44:19 +0900

    #8 dekita

--- question/question/views.py
+++ question/question/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import QuestionModel
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class QuestionList(generic.ListView):
+    template_name = 'question/list.html'
+    model = QuestionModel
+
+class QuestionDetail(generic.DetailView):
+    template_name = 'question/detail.html'
+    model = QuestionModel
+
+class QuestionUpdate(generic.UpdateView):
+    template_name = 'question/update.html'
+    model = QuestionModel
+    fields = ('title', 'content', 'deadline')
+    success_url = reverse_lazy('list')
+
+class QuestionDelete(generic.DeleteView):
+    template_name = 'question/delete.html'
+    model = QuestionModel
+
+    success_url = reverse_lazy('list')
+
+class QuestionCreate(generic.CreateView):
+    template_name = 'Question/create.html'
+    model = QuestionModel
+    fields = ('title', 'content', 'deadline')
+
+    success_url = reverse_lazy('list')
