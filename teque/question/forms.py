afa188d3048f97ef5866b622b94f000701908d55
msuahsi-0130 <musashi@MacBook-Pro.local>
2021-03-29 19:24:47 +0900

    #44 addlogin

--- teque/question/forms.py
+++ teque/question/forms.py
@@ -3,13 +3,11 @@ from PIL import Image
 
 
 class UpLoadImgForm(forms.Form):
-    '''
     image_1 = forms.QuestionModel(required=True)
     image_2 = forms.QuestionModel(required=True)
     image_3 = forms.QuestionModel(required=True)
     image_4 = forms.QuestionModel(required=True)
-    '''
-
+    
 class CategoryForm(forms.Form):
     choice1 = forms.fields.ChoiceField(
         required=True,
